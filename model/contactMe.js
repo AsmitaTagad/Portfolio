f01fc2a977e36272e3984cae5fc9992cccfcb3d5
Asmita Tagad <asmitatagad07@gmail.com>
2025-10-22 10:13:28 +0530

    chnages

--- model/contactMe.js
+++ model/contactMe.js
@@ -1,10 +1,15 @@
 import { model, Schema } from "mongoose";
 
-const contactSchema = new Schema({
-  name: { type: String },
-  email: { type: String},
-  subject: { type: String },
-  message: { type: String },
-});
+const contactSchema = new Schema(
+  {
+    name: { type: String },
+    email: { type: String },
+    subject: { type: String },
+    message: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export default model("contactSchema", contactSchema);
